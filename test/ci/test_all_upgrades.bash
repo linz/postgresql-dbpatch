32e31b129d13237eead636297ed342a47b454a49
Victor Engmark <vengmark@linz.govt.nz>
2022-01-18 14:51:04 +1300

    refactor: Get rid of semicolons

--- test/ci/test_all_upgrades.bash
+++ test/ci/test_all_upgrades.bash
@@ -8,7 +8,7 @@ cd "$(dirname "$0")/../../"
 #
 # Versions/tags known to build
 #
-VER="1.0.0 1.0.1 1.1.0 1.3.0 1.4.0 1.5.0 1.6.0 1.7.0";
+VER="1.0.0 1.0.1 1.1.0 1.3.0 1.4.0 1.5.0 1.6.0 1.7.0"
 
 # Install all older versions
 if [ -e .git/shallow ]
@@ -18,19 +18,25 @@ fi
 git fetch --tags
 git clone . older-versions
 cd older-versions
-for v in $VER; do
+for v in $VER
+do
   echo "-------------------------------------"
   echo "Installing version $v"
   echo "-------------------------------------"
   git clean -dxf && git checkout "$v" && sudo env "PATH=$PATH" make install || exit 1
-done;
+done
 cd ..
-rm -rf older-versions;
+rm -rf older-versions
 
 # Test upgrade from all older versions
-for v in $VER; do
+for v in $VER
+do
   echo "-------------------------------------"
   echo "Checking upgrade from version $v"
   echo "-------------------------------------"
-  make installcheck-upgrade PREPAREDB_UPGRADE_FROM="$v" || { cat regression.diffs; exit 1; }
+  if ! make installcheck-upgrade PREPAREDB_UPGRADE_FROM="$v"
+  then
+      cat regression.diffs
+      exit 1
+  fi
 done
